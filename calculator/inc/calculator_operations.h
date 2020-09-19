1df71cf0737bfc610b6a10ad86677f9255eed7e9
stepin104475 <69069240+stepin104475@users.noreply.github.com>
2020-09-19 08:59:31 +0530

    Add files via upload

---
+++ calculator/inc/calculator_operations.h
@@ -0,0 +1,130 @@
+/**
+* @file calculator_operations.h
+* Calculator application with 4 mathematical operations
+*
+*/
+#ifndef __CALCULATOR_OPERATIONS_H__
+#define __CALCULATOR_OPERATIONS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+
+/**
+*  adds the operand1 and operand2 and returns the result
+* @param[in] operand1
+* @param[in] operand2
+* @return Result of operand1 + operand2
+*/
+int addition(int operand1, int operand2);
+
+/**
+*  subtracts the operand1, operand2 and returns the result
+* @param[in] operand1
+* @param[in] operand2
+* @return Result of operand1 - operand2
+*/
+int subtraction(int operand1, int operand2);
+
+/**
+*  multiply the operand1, operand2 and returns the result
+* @param[in] operand1
+* @param[in] operand2
+* @return Result of operand1 * operand2
+*/
+int multiplication(int operand1, int operand2);
+
+
+/**
+* divides the operand1 by operand2 and returns the result
+* @param[in] operand1
+* @param[in] operand2
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+int division(int operand1, int operand2);
+/**
+* finds sine function value and returns the result
+* @param[in] operand1
+* @return float value of the sine(operand1)
+*/
+float sine(float  operand1);
+/**
+* finds cosine function value and returns the result
+* @param[in] operand1
+* @return float value of the cosine(operand1)
+*/
+
+float cosine(float operand1);
+/**
+* finds tangent function value and returns the result
+* @param[in] operand1
+* @return float value of the tangent(operand1)
+*/
+
+float tangent(float x);
+/**
+* finds sineh function value and returns the result
+* @param[in] operand1
+* @return float value of the sinh(operand1)
+*/
+
+float sineh(float x);
+/**
+* finds cosineh function value and returns the result
+* @param[in] operand1
+* @return float value of the cosineh(operand1)
+*/
+
+float cosineh(float x);
+\/**
+* finds tangenth function value and returns the result
+* @param[in] operand1
+* @return float value of the tangenth(operand1)
+*/
+
+float tangenth(float x);
+/**
+* finds logten function value and returns the result
+* @param[in] operand1
+* @return float value of the logten(operand1)
+*/
+
+float logten(float x);
+/**
+* finds squareroot function value and returns the result
+* @param[in] operand1
+* @return float value of the squareroot(operand1)
+*/
+
+float squareroot(float x);
+/**
+* finds exponent function value and returns the result
+* @param[in] operand1
+* @return float value of the exponent(operand1)
+*/
+
+float exponent(float operand1);
+/**
+* finds power function value and returns the result
+* @param[in] operand1
+* @return float value of the power(operand1)
+*/
+
+float power(float x,float y);
+/**
+* finds factorial function value and returns the result
+* @param[in] operand1
+* @return float value of the factorial(operand1)
+*/
+int factorial(int x);
+/**
+* finds absolute function value and returns the result
+* @param[in] operand1
+* @return float value of the absolute(operand1)
+*/
+
+float absolute(float x,float y);
+
+
+#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
---
+++ calculator/project_main.c
@@ -0,0 +1,218 @@
+#include <calculator_operations.h>
+
+/* Status of the operation requested */
+#define VALID   (1)
+#define INVALID (0)
+
+/* Calculator operation requested by user*/
+unsigned int calculator_operation = 0;
+
+/* Operands on which calculation is performed */
+float calculator_operand1 = 0;
+float calculator_operand2 = 0;
+int calculator_operand3=0;
+
+/* Valid operations */
+enum operations{ sine=1, cosine, tangent,sineh,cosineh,tangenth,logten,squareroot,exponent,addition,subtraction,multiplication,division,modulus,factorial, EXIT };
+
+/* Display the menu of operations supported */
+void calculator_menu(void);
+/* Verifies the requested operations validity */
+int valid_operation(int operation);
+
+
+/* Start of the application */
+int main(int argc, char *argv[])
+{
+    printf("\n****Calculator****\n");
+    while(1)
+    {
+        calculator_menu();
+    }
+}
+
+void calculator_menu(void)
+{
+    printf("\nAvailable Operations\n");
+    printf("\n1. sine\n2. cosine\n3. tangent\n4. sineh\n5. cosineh\n6. tangenth\n7. logten\n8. squareroot\n9. exponent\n10. addition\n11. subtraction\n12. multiplication\n13. division\n14. modulus\n15.power\n16.factorial\n17. Exit");
+    printf("\n\tEnter your choice\n");
+
+     __fpurge(stdin);
+    scanf("%d", &calculator_operation);
+
+    if(EXIT == calculator_operation)
+    {
+        printf("\nThank you. Exiting the Application\n");
+        exit(0);
+    }
+
+    if(INVALID != valid_operation(calculator_operation))
+    {
+        if(n<9 && n>0){
+        printf("\n\tEnter your Numbers with space between them\n");
+        __fpurge(stdin);
+        scanf("%f", &calculator_operand1);
+        }
+        else if(n>9 && n<16){
+             printf("\n\tEnter your Numbers with space between them\n");
+        __fpurge(stdin);
+        scanf("%f %f", &calculator_operand1, &calculator_operand2);
+        }
+        else if(n==16){
+         printf("\n\tEnter your Numbers with space between them\n");
+        __fpurge(stdin);
+        scanf("%d", &calculator_operand3);
+}
+    }
+    else
+    {
+        printf("\n\t---Wrong choice---\nEnter to continue\n");
+        __fpurge(stdin);
+        getchar();
+        return;
+
+    }
+    switch(calculator_operation)
+    {
+        case 1:
+            printf("\n\t%d + %d = %d\nEnter to continue",
+            calculator_operand1,
+            sine(calculator_operand1));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case 2:
+            printf("\n\t%d + %d = %d\nEnter to continue",
+            calculator_operand1,
+            cosine(calculator_operand1));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case 3:
+            printf("\n\t%d + %d = %d\nEnter to continue",
+            calculator_operand1,
+            tangent(calculator_operand1));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case 4:
+            printf("\n\t%d + %d = %d\nEnter to continue",
+            calculator_operand1,
+            sineh(calculator_operand1));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case 5:
+            printf("\n\t%d + %d = %d\nEnter to continue",
+            calculator_operand1,
+            cosineh(calculator_operand1));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case 6:
+            printf("\n\t%d + %d = %d\nEnter to continue",
+            calculator_operand1,
+            tangenth(calculator_operand1));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case 7:
+            printf("\n\t%d + %d = %d\nEnter to continue",
+            calculator_operand1,
+            logten(calculator_operand1));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case 8:
+            printf("\n\t%d + %d = %d\nEnter to continue",
+            calculator_operand1,
+            squareroot(calculator_operand1));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case 9:
+            printf("\n\t%d + %d = %d\nEnter to continue",
+            calculator_operand1,
+            exponent(calculator_operand1));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case 10:
+            printf("\n\t%d + %d = %d\nEnter to continue",
+            calculator_operand1,
+            addition(calculator_operand1,calculator_operand2));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case 11:
+            printf("\n\t%d + %d = %d\nEnter to continue",
+            calculator_operand1,
+            subtraction(calculator_operand1,calculator_operand2));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case 12:
+            printf("\n\t%d + %d = %d\nEnter to continue",
+            calculator_operand1,
+            multiplication(calculator_operand1,calculator_operand2));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case 13:
+            printf("\n\t%d + %d = %d\nEnter to continue",
+            calculator_operand1,
+            division(calculator_operand1,calculator_operand2));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case 14:
+            printf("\n\t%d + %d = %d\nEnter to continue",
+            calculator_operand1,
+            modulus(calculator_operand1,calculator_operand2));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case 15:
+            printf("\n\t%d + %d = %d\nEnter to continue",
+            calculator_operand1,
+            power(calculator_operand1,calculator_operand2));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case 16:
+            printf("\n\t%d + %d = %d\nEnter to continue",
+            calculator_operand1,
+            factorial(calculator_operand3));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+
+        case 17:
+            exit(0);
+            break;
+        default:
+            printf("\n\t---It should never come here---\n");
+    }
+}
+
+int valid_operation(int operation)
+{
+    /* Check if the operation is a valid operation */
+    return ((sine <= operation) && (EXIT >= operation)) ? VALID: INVALID;
+}
