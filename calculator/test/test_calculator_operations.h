84bdeee22aa14bba588a523bd7b071f2799e4052
stepin104475 <69069240+stepin104475@users.noreply.github.com>
2020-09-19 09:13:52 +0530

    Update test_calculator_operations.h

--- calculator/test/test_calculator_operations.h
+++ calculator/test/test_calculator_operations.h
@@ -10,6 +10,20 @@ void test_add(void);
 void test_subtract(void);
 void test_multiply(void);
 void test_divide(void);
+void test_modulus(void);
+void test_primenumber(void);
+void test_greater(void);
+void test_smaller(void);
+void test_square(void);
+void test_and(void);
+void test_or(void);
+void test_not(void);
+void test_nand(void);
+void test_nor(void);
+void test_factorial(void);
+void test_adder3bit(void);
+void test_subtractor3bit(void);
+
 
 /* Start of the application test */
 int main() {
@@ -26,7 +40,19 @@ int main() {
   CU_add_test(suite, "subtract", test_subtract);
   CU_add_test(suite, "multiply", test_multiply);
   CU_add_test(suite, "divide", test_divide);
-
+  CU_add_test(suite, "modulus", test_modulus);
+  CU_add_test(suite, "primenumber", test_primenumber);
+  CU_add_test(suite, "greater", test_greater);
+  CU_add_test(suite, "smaller", test_smaller);
+  CU_add_test(suite, "square", test_square);
+  CU_add_test(suite, "and", test_and);
+  CU_add_test(suite, "or", test_or);
+  CU_add_test(suite, "not", test_not);
+  CU_add_test(suite, "and", test_nand);
+  CU_add_test(suite, "or", test_nor);
+  CU_add_test(suite, "factorial", test_factorial);
+  CU_add_test(suite, "adder3bit", test_adder3bit);
+  CU_add_test(suite, "subtractor3bit", test_subtractor3bit);
 
 /* Note: Do not edit START*/
   /* Setup Test Framework to output the result to Screen */
@@ -69,4 +95,81 @@ void test_divide(void) {
   /* Dummy fail*/
   CU_ASSERT(3 == divide(2, 2));
 }
-
+void test_modulus(void) {
+  CU_ASSERT(3 == divide(10, 7));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1 == divide(2, 2));
+}
+void test_primenumber(void) {
+  CU_ASSERT(1 == primenumber(5));
+  
+  /* Dummy fail*/
+  CU_ASSERT(0 == primenumber(2));
+}
+void test_greater(void) {
+  CU_ASSERT(10 == greater(10, 7));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1 == greater(2, 1));
+}
+void test_smaller(void) {
+  CU_ASSERT(7 == smaller(10, 7));
+  
+  /* Dummy fail*/
+  CU_ASSERT(4 == divide(4, 2));
+}
+void test_square(void) {
+  CU_ASSERT(49 == square(7));
+  
+  /* Dummy fail*/
+  CU_ASSERT(5 == square(2));
+}
+void test_and(void) {
+  CU_ASSERT(1 == and(1,1));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1 == and(1,0));
+}
+void test_or(void) {
+  CU_ASSERT(1 == or(1,0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(0 == or(1,1));
+}
+void test_not(void) {
+  CU_ASSERT(1 == not(0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(0 == not(0));
+}
+void test_nand(void) {
+  CU_ASSERT(0 == and(1,1));
+  
+  /* Dummy fail*/
+  CU_ASSERT(0 == and(1,0));
+}
+void test_nor(void) {
+  CU_ASSERT(0 == or(1,0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1 == or(1,1));
+}
+void test_factorial(void) {
+  CU_ASSERT(12 == factorial(4));
+  
+  /* Dummy fail*/
+  CU_ASSERT(5 == factorial(2));
+}
+void test_adder3bit(void) {
+  CU_ASSERT(0 == adder3bit(1,1,0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1 == adder3bit(1,0,0));
+}
+void test_subtractor3bit(void) {
+  CU_ASSERT(0 == adder3bit(1,1,0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1 == adder3bit(1,0,0));
+}
