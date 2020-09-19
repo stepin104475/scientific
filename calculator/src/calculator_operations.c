bda33403546f38b9b06d1380c38b04f5230fdd80
stepin104475 <69069240+stepin104475@users.noreply.github.com>
2020-09-19 09:06:15 +0530

    Update calculator_operations.c

--- calculator/src/calculator_operations.c
+++ calculator/src/calculator_operations.c
@@ -22,3 +22,44 @@ int divide(int operand1, int operand2)
     else
         return operand1 / operand2;
 }
+float cosine(float operand1)
+{
+    return (cos (operand1*PI/180));
+}
+float tangent(float operand1)
+{
+    return (tan(operand1*PI/180));
+}
+float sineh(float operand1)
+{
+    return (sinh(operand1));
+}
+float cosineh(float operand1)
+{
+    return (sinh(operand1));
+}
+float tangenth(float operand1)
+{
+    return (sinh(operand1));
+}
+float logten(float operand1)
+{
+    return (log10(operand1));
+}
+float squareroot(float operand1)
+{
+    return (sqrt(operand1));
+}
+float exponent(float operand1)
+{
+    return(exp(x));
+}
+float power(float operand1, float operand2)
+{
+    return (pow(operand1,operand2));
+}
+float absolute(float operand1,float operand2)
+{
+
+    return abs(operand1-operand2);
+}
