ee04e5dccf717f2d70a1924d8cef5769eb8f0b04
stepin104475 <69069240+stepin104475@users.noreply.github.com>
2020-09-19 09:09:11 +0530

    Update calculator_operations.c

--- calculator/src/calculator_operations.c
+++ calculator/src/calculator_operations.c
@@ -52,7 +52,7 @@ float squareroot(float operand1)
 }
 float exponent(float operand1)
 {
-    return(exp(x));
+    return(exp(operand1));
 }
 float power(float operand1, float operand2)
 {
@@ -63,3 +63,71 @@ float absolute(float operand1,float operand2)
 
     return abs(operand1-operand2);
 }
+int primenumber(int operand1)
+    {
+        int i,count=0;
+        if(operand1<0)
+        {
+            return -1;
+        }
+        for(i=1;i<=operand1;i++)
+        {
+            if(operand1%i==0)
+            {
+                count++;
+            }
+        }
+        if(count==2)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+int greater(int operand1, int operand2)
+{
+    if(operand1 > operand2)
+        return operand1;
+    else
+        return operand2;
+}
+int smaller(int operand1, int operand2)
+{
+    if(operand1 < operand2)
+        return operand1;
+    else
+        return operand2;
+}
+int square(int operand1)
+{
+    int result;
+    result= operand1 * operand1;
+    return result;
+}	
+int and(int operand1, int operand2)
+{
+    int result;
+    if(operand1<0 && operand2<0)
+    return -1;
+    else if(operand1>1 && operand2>1)
+    return 1;
+    else if(operand1==1 && operand2==1)
+    return 1;
+    else
+    return 0;
+}
+int or(int operand1, int operand2)
+{
+    int result;
+    if(operand1<0 && operand2<0)
+    return -1;
+    else if(operand1>1 && operand2>1)
+    return 1;
+    else if(operand1==0 && operand2==0)
+    return 0;
+    else
+    return 1;
+}
+
